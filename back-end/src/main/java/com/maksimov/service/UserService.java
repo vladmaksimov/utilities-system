313d456349cd02885a654be5ad99003eb5c6179e
v.maksimov <vlad_maksimov@mail.ru>
2016-04-24 18:12:37 +0300

    Added basic xml configuration

--- back-end/src/main/java/com/maksimov/service/UserService.java
+++ back-end/src/main/java/com/maksimov/service/UserService.java
@@ -2,6 +2,7 @@ package com.maksimov.service;
 
 import com.maksimov.persistence.UserPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     @Autowired
+    @Qualifier(value = "userPersistence")
     private UserPersistence userPersistence;
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
